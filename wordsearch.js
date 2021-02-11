1bcb849f43218592bbdf559f8c4211d873b1ae3b
ermurray <ermurray31@gmail.com>
2021-02-11 05:15:31 +0000

    added diagonal logic

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,8 @@ const wordSearch = (letters, word) => {
   if (letters.length > 0 && word !== undefined) {
     const horizontalJoin = letters.map(ls => ls.join(''));
     const trLetters = transpose(letters);
+    const rotate45Letters = rotate45(letters);
+    const diagonalJoin = rotate45Letters.map(dl => dl.join(''));
     const verticalJoin = trLetters.map(cl => cl.join(''));
     const reverseWord = word.split('').reverse().join('');
     let result = false;
@@ -15,6 +17,10 @@ const wordSearch = (letters, word) => {
       if (c.includes(word)|| c.includes(reverseWord)) {
         result = true;
       }
+    }
+    for (const d of diagonalJoin) {
+      if (d.includes(word) || d.includes(reverseWord))
+        result = true;
     }
   
     return result;
@@ -38,5 +44,26 @@ const transpose = function(matrix) {
     }
   }
   return newMatrix;
+};
+const rotate45 = function(matrix) {
+  let n = matrix.length; //hieght
+  let m = matrix[0].length; //width
+  let max = Math.max(n,m);
+  let subArray;
+  let rotatedArray = [];
+  for (let i = 0; i < 2 * max - 1; i++) { // loop for enough times to get all acceptable index combos. trial and error with m and n and 2 times m or n
+    subArray = [];
+    for (let j = (n - 1); j >= 0; j--) {
+      let x = i - j;                      // got logic from stack overflow to create conditional of which index combos worked (how to traverse array diagonally)
+      
+      if (x >= 0 && x < m) {
+        subArray.push(matrix[j][x]);
+      }
+    }
+    if (subArray.length > 0) {
+      rotatedArray.push(subArray);
+    }
+  }
+  return rotatedArray;
 };
 module.exports = wordSearch;
